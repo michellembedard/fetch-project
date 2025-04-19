fbb3e4130cd98c0e659c9a49e6287958195b9542
michellembedard <michellembedard16@gmail.com>
2025-04-19 10:02:28 -0500

    plotting to visualize data

--- src/part1__data_exploration.py
+++ src/part1__data_exploration.py
@@ -2,6 +2,9 @@
 # import packages
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+
 
 # %%
 # set up date fields
@@ -16,20 +19,20 @@ transactions_df = pd.read_csv(
 users_df = pd.read_csv("../data/USER_TAKEHOME.csv", parse_dates=users_parse_dates)
 # %%
 # verify correct data type imports
-products_df.dtypes
-transactions_df.dtypes
-users_df.dtypes
+print(products_df.dtypes)
+print(transactions_df.dtypes)
+print(users_df.dtypes)
 
 # %%
 # look for missing values
-products_df.info()
-transactions_df.info()
-users_df.info()
+print(products_df.info())
+print(transactions_df.info())
+print(users_df.info())
 
 # %%
 # fix transactions_df numeric formatting
-transactions_df["FINAL_QUANTITY"].value_counts()
-transactions_df["FINAL_SALE"].value_counts()
+print(transactions_df["FINAL_QUANTITY"].value_counts())
+print(transactions_df["FINAL_SALE"].value_counts())
 # FINAL_QUANTITY "zero" should be "0", then convert to number
 # FINAL_SALE should remove " ", then convert to number
 
@@ -38,10 +41,33 @@ transactions_df["FINAL_QUANTITY"] = transactions_df["FINAL_QUANTITY"].str.replac
     "zero", "0"
 )
 transactions_df["FINAL_QUANTITY"].replace("", np.nan, inplace=True)
-transactions_df["FINAL_QUANTITY"].astype(float)
+transactions_df["FINAL_QUANTITY"] = transactions_df["FINAL_QUANTITY"].astype(float)
 
 ## fix final_sale
 transactions_df["FINAL_SALE"] = transactions_df["FINAL_SALE"].str.replace(" ", "")
 transactions_df["FINAL_SALE"].replace("", np.nan, inplace=True)
-transactions_df["FINAL_SALE"].astype(float)
+transactions_df["FINAL_SALE"] = transactions_df["FINAL_SALE"].astype(float)
+# %%
+# look at value distribution for each df
+
+# Indicate we want to add a column for missing data
+SHOW_MISSING = True
+
+# for each df, run through the columns
+for d in [products_df, transactions_df, users_df]:
+    for c in list(d.columns):
+        if SHOW_MISSING:
+            d["filled"] = d[c].fillna("MISSING")
+        else:
+            d["filled"] = d[c]
+        # create a histogram, sorted by quantity, to visualize our data
+        fig = go.Figure()
+        fig.add_trace(
+            go.Histogram(histfunc="count", y=d.index, x=d["filled"], name="count")
+        )
+        fig.update_xaxes(categoryorder="total descending")
+        fig.update_layout(title=str(c))
+        fig.show()
+
+
 # %%
