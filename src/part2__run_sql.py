924794cb2091514ea6e62578e2b90832fe247349
michellembedard <michellembedard16@gmail.com>
2025-04-19 12:22:40 -0500

    setting up data for running sql

---
+++ src/part2__run_sql.py
@@ -0,0 +1,15 @@
+# %%
+import duckdb
+import pandas as pd
+
+# %%
+# import csvs as dfs so that we can run the sql below
+users_parse_dates = ["CREATED_DATE", "BIRTH_DATE"]
+transaction_parse_dates = ["PURCHASE_DATE", "SCAN_DATE"]
+
+# import data
+products_df = pd.read_csv("../data/PRODUCTS_TAKEHOME.csv")
+transactions_df = pd.read_csv(
+    "../data/TRANSACTION_TAKEHOME.csv", parse_dates=transaction_parse_dates
+)
+users_df = pd.read_csv("../data/USER_TAKEHOME.csv", parse_dates=users_parse_dates)
