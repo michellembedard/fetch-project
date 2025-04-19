c163c75d1b40f8099e7f53be13152245cee3b21c
michellembedard <michellembedard16@gmail.com>
2025-04-19 12:23:47 -0500

    setting up problem 1 assumptions

--- src/part2__run_sql.py
+++ src/part2__run_sql.py
@@ -13,3 +13,24 @@ transactions_df = pd.read_csv(
     "../data/TRANSACTION_TAKEHOME.csv", parse_dates=transaction_parse_dates
 )
 users_df = pd.read_csv("../data/USER_TAKEHOME.csv", parse_dates=users_parse_dates)
+
+#%%
+#Close-ended question 1:
+#1. What are the top 5 brands by receipts scanned among users 21 and over?
+
+#Assumptions: I only have a sample of data.
+# The code is set up to run properly if more data is present
+
+#Assumptions: We want the top 5 results. 
+# However, if there is a tie for 5th place, include all brands which are tied for the 5th place spot, 
+# as there is is not an inherent order to brands
+
+#Assumptions: We care about unique receipts scanned, not specific times the brand was on the same receipt 
+# This allows for us to not account for the missing quantity data, which we cannot resolve without understanding business assumptions
+# And we believe it is more important for the item to be purchased multiple times in distinct trips to the store, rather than multiples within the same check-out.
+
+d1 = open('../sql/close_ended_1.sql', 'r')
+sql1= d1.read()
+d1.close()
+duckdb.sql(sql1).show()
+
