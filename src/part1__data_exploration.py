793fa51d962da9d1af5c7c91b7515d2a4db33aed
michellembedard <michellembedard16@gmail.com>
2025-04-19 09:48:01 -0500

    verifying and fixing data formats

--- src/part1__data_exploration.py
+++ src/part1__data_exploration.py
@@ -14,3 +14,34 @@ transactions_df = pd.read_csv(
     "../data/TRANSACTION_TAKEHOME.csv", parse_dates=transaction_parse_dates
 )
 users_df = pd.read_csv("../data/USER_TAKEHOME.csv", parse_dates=users_parse_dates)
+# %%
+# verify correct data type imports
+products_df.dtypes
+transactions_df.dtypes
+users_df.dtypes
+
+# %%
+# look for missing values
+products_df.info()
+transactions_df.info()
+users_df.info()
+
+# %%
+# fix transactions_df numeric formatting
+transactions_df["FINAL_QUANTITY"].value_counts()
+transactions_df["FINAL_SALE"].value_counts()
+# FINAL_QUANTITY "zero" should be "0", then convert to number
+# FINAL_SALE should remove " ", then convert to number
+
+## fix final_quantity
+transactions_df["FINAL_QUANTITY"] = transactions_df["FINAL_QUANTITY"].str.replace(
+    "zero", "0"
+)
+transactions_df["FINAL_QUANTITY"].replace("", np.nan, inplace=True)
+transactions_df["FINAL_QUANTITY"].astype(float)
+
+## fix final_sale
+transactions_df["FINAL_SALE"] = transactions_df["FINAL_SALE"].str.replace(" ", "")
+transactions_df["FINAL_SALE"].replace("", np.nan, inplace=True)
+transactions_df["FINAL_SALE"].astype(float)
+# %%
